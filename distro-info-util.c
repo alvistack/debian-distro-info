f467083219f4769fe3ae02465b98f538f8b4572e
Benjamin Drung <bdrung@debian.org>
2014-05-10 20:55:06 +0200

    Update year in copyright.

--- distro-info-util.c
+++ distro-info-util.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2013, Benjamin Drung <bdrung@debian.org>
+ * Copyright (C) 2012-2014, Benjamin Drung <bdrung@debian.org>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
