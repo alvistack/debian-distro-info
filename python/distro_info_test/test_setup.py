3943bd3e5d3db08814d91cc15ffa0035fe7e8e6e
Benjamin Drung <bdrung@debian.org>
2023-01-27 18:35:10 +0100

    make_pep440_compliant: Handle more Debian versions correctly
    Handle more Debian versions correctly in `make_pep440_compliant` that
    include tilde and/or plus signs. For example:
    
    * 0.21~bpo9+1
    * 2.21.3+deb11u1
    * 2.21.3+deb11u1~bpo10+1
    * 0.175~18.04.3
    * 2.22.1ubuntu1~bpo20.04.1
    
    Signed-off-by: Benjamin Drung <bdrung@debian.org>

--- python/distro_info_test/test_setup.py
+++ python/distro_info_test/test_setup.py
@@ -27,6 +27,32 @@ class SetupTestCase(unittest.TestCase):
         """Test make_pep440_compliant() with already correct version."""
         self.assertEqual(make_pep440_compliant("1.2"), "1.2")
 
+    def test_make_pep440_compliant_debian_backport(self) -> None:
+        """Test make_pep440_compliant() with Debian backport version."""
+        self.assertEqual(make_pep440_compliant("0.21~bpo9+1"), "0.21+bpo9.1")
+
+    def test_make_pep440_compliant_debian_stable(self) -> None:
+        """Test make_pep440_compliant() with Debian stable update."""
+        self.assertEqual(make_pep440_compliant("2.21.3+deb11u1"), "2.21.3+deb11u1")
+
+    def test_make_pep440_compliant_debian_stable_backport(self) -> None:
+        """Test make_pep440_compliant() with Debian stable backport."""
+        self.assertEqual(make_pep440_compliant("2.21.3+deb11u1~bpo10+1"), "2.21.3+deb11u1.bpo10.1")
+
+    def test_make_pep440_compliant_tilde(self) -> None:
+        """Test make_pep440_compliant() with tilde in Debian version."""
+        self.assertEqual(make_pep440_compliant("0.175~18.04.3"), "0.175+18.04.3")
+
     def test_make_pep440_compliant_ubuntu(self) -> None:
         """Test make_pep440_compliant() with Ubuntu version."""
         self.assertEqual(make_pep440_compliant("1.1ubuntu1"), "1.1+ubuntu1")
+
+    def test_make_pep440_compliant_ubuntu_backport(self) -> None:
+        """Test make_pep440_compliant() with Ubuntu backport version."""
+        self.assertEqual(
+            make_pep440_compliant("2.22.1ubuntu1~bpo20.04.1"), "2.22.1+ubuntu1.bpo20.04.1"
+        )
+
+    def test_make_pep440_compliant_ubuntu_security(self) -> None:
+        """Test make_pep440_compliant() with Ubuntu security update."""
+        self.assertEqual(make_pep440_compliant("2.17.12ubuntu1.1"), "2.17.12+ubuntu1.1")
