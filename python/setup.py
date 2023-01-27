1ddd87e08e1f5c7ab9ef97acdb4ffe4efd578b58
Benjamin Drung <bdrung@debian.org>
2023-01-27 18:35:10 +0100

    make_pep440_compliant: Handle more Debian versions correctly
    Handle more Debian versions correctly in `make_pep440_compliant` that
    include tilde and/or plus signs. For example:
    
    * 0.21~bpo9+1
    * 2.21.3+deb11u1
    * 2.21.3+deb11u1~bpo10+1
    * 0.175~18.04.3
    * 2.22.1ubuntu1~bpo20.04.1
    
    Signed-off-by: Benjamin Drung <bdrung@debian.org>

--- python/setup.py
+++ python/setup.py
@@ -25,10 +25,13 @@ def get_debian_version():
 
 def make_pep440_compliant(version: str) -> str:
     """Convert the version into a PEP440 compliant version."""
-    pep440_version = re.sub("([a-zA-Z])", r"+\1", version, count=1)
-    assert re.match(
-        r"^[0-9][0-9.]*((a|b|rc|.post|.dev)[0-9]+)*(\+[a-zA-Z0-9.]+)?$", pep440_version
-    ), f"'{pep440_version}' not PEP440 compliant"
+    public_version_re = re.compile(r"^([0-9][0-9.]*(?:(?:a|b|rc|.post|.dev)[0-9]+)*)\+?")
+    _, public, local = public_version_re.split(version, maxsplit=1)
+    if not local:
+        return version
+    sanitized_local = re.sub("[+~]+", ".", local).strip(".")
+    pep440_version = f"{public}+{sanitized_local}"
+    assert re.match("^[a-zA-Z0-9.]+$", sanitized_local), f"'{pep440_version}' not PEP440 compliant"
     return pep440_version
 
 
