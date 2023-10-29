5afcb5970b8b83b33824d3098df831f032d6ba31
Stefano Rivera <stefanor@debian.org>
2023-10-29 23:45:57 +0200

    Make the Python module version PEP440 compliant.

--- python/setup.py
+++ python/setup.py
@@ -22,9 +22,22 @@ SCRIPTS = [
     'ubuntu-distro-info',
 ]
 
+def make_pep440_compliant(version):
+    """Convert the version into a PEP440 compliant version."""
+    public_version_re = re.compile(r"^([0-9][0-9.]*(?:(?:a|b|rc|.post|.dev)[0-9]+)*)\+?")
+    _, public, local = public_version_re.split(version, maxsplit=1)
+    if not local:
+        return version
+    sanitized_local = re.sub("[+~]+", ".", local).strip(".")
+    pep440_version = "{}+{}".format(public, sanitized_local)
+    assert re.match("^[a-zA-Z0-9.]+$", sanitized_local), (
+        "'{}' not PEP440 compliant".format(pep440_version))
+    return pep440_version
+
+
 if __name__ == '__main__':
     setup(name='distro-info',
-          version=get_debian_version(),
+          version=make_pep440_compliant(get_debian_version()),
           py_modules=['distro_info'],
           packages=['distro_info_test'],
           test_suite='distro_info_test.discover',
